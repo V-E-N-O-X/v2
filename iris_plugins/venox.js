c2501906bbac63a9a46e8136a77273fcb8bac8ee
~TʜᴇFʟᴀᴍᴇX <138563418+V-E-N-O-X@users.noreply.github.com>
2024-04-18 20:46:56 +0600

    Update venox.js

--- iris_plugins/venox.js
+++ iris_plugins/venox.js
@@ -69,3 +69,171 @@ try {
   low = require("./lib/lowdb");
 }
 //
+const { Low, JSONFile } = low;
+const mongoDB = require("./lib/mongoDB");
+//
+global.opts = new Object(
+  yargs(process.argv.slice(2)).exitProcess(false).parse()
+);
+global.db = new Low(
+  /https?:\/\//.test(opts["db"] || "")
+    ? new cloudDBAdapter(opts["db"])
+    : /mongodb/.test(opts["db"])
+      ? new mongoDB(opts["db"])
+      : new JSONFile(`/lib/external/database.json`)
+);
+global.DATABASE = global.db;
+global.loadDatabase = async function loadDatabase() {
+  if (global.db.READ)
+    return new Promise((resolve) =>
+      setInterval(function () {
+        !global.db.READ
+          ? (clearInterval(this),
+            resolve(
+              global.db.data == null ? global.loadDatabase() : global.db.data
+            ))
+          : null;
+      }, 1 * 1000)
+    );
+  if (global.db.data !== null) return;
+  global.db.READ = true;
+  await global.db.read();
+  global.db.READ = false;
+  global.db.data = {
+    users: {},
+    chats: {},
+    database: {},
+    game: {},
+    settings: {},
+    others: {},
+    sticker: {},
+    ...(global.db.data || {}),
+  };
+  global.db.chain = _.chain(global.db.data);
+};
+loadDatabase();
+global.db = JSON.parse(fs.readFileSync("./lib/external/database.json"));
+if (global.db)
+  global.db = {
+    sticker: {},
+    database: {},
+    game: {},
+    others: {},
+    users: {},
+    ...(global.db || {}),
+  };
+
+
+// ====== Imported Modules ====== //
+let isSleeping = false;
+let banUser = JSON.parse(fs.readFileSync('./lib/database/banUser.json'));
+let banchat = JSON.parse(fs.readFileSync('./lib/database/banChat.json'));
+let venoxaudio = JSON.parse(fs.readFileSync('./lib/audio/audio.json'));
+let _limit = JSON.parse(fs.readFileSync('./lib/storage/user/limit.json'));
+let _buruan = JSON.parse(fs.readFileSync('./lib/storage/user/bounty.json'));
+let _darahOrg = JSON.parse(fs.readFileSync('./lib/storage/user/blood.json'))
+let ntnsfw = JSON.parse(fs.readFileSync('./lib/database/nsfw.json'));
+let pendaftar = JSON.parse(fs.readFileSync('./lib/storage/user/user.json'))
+let balance = JSON.parse(fs.readFileSync('./lib/database/balance.json'))
+let ssewa = JSON.parse(fs.readFileSync('./lib/database/sewa.json'))
+let ban = JSON.parse(fs.readFileSync('./lib/database/ban.json'))
+let autosticker = JSON.parse(fs.readFileSync('./lib/database/autosticker.json'))
+const _autostick = JSON.parse(fs.readFileSync('./lib/database/autostickpc.json'))
+let limit = JSON.parse(fs.readFileSync('./lib/database/limit.json'))
+let setik = JSON.parse(fs.readFileSync('./lib/external/sticker.json'))
+let vien = JSON.parse(fs.readFileSync('./lib/external/audio.json'))
+let imagi = JSON.parse(fs.readFileSync('./lib/external/image.json'))
+let videox = JSON.parse(fs.readFileSync('./lib/external/video.json'))
+global.db = JSON.parse(fs.readFileSync('./lib/external/database.json'))
+let _sewa = require("./lib/sewa");
+const sewa = JSON.parse(fs.readFileSync('./lib/database/sewa.json'))
+
+
+// ====== Modules ====== //
+const time = moment.tz('Asia/Dhaka').format('DD/MM HH:mm:ss')
+const ucap = moment(Date.now()).tz('Asia/Dhaka').locale('id').format('a')
+var buln = ['/01/', '/02/', '/03/', '/04/', '/05/', '/06/', '/07/', '/08/', '/09/', '/10/', '/11/', '/12/'];
+var myHari = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var tgel = new Date();
+var hri = tgel.getDate();
+var bulnh = tgel.getMonth();
+var thisHari = tgel.getDay(),
+  thisDaye = myHari[thisHari];
+var yye = tgel.getYear();
+var syear = (yye < 1000) ? yye + 1900 : yye;
+const jangwak = (hri + '' + buln[bulnh] + '' + syear)
+const janghar = (thisDaye)
+var myHari = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+var tgel = new Date();
+var thisHari = tgel.getDay(),
+  thisDaye = myHari[thisHari];
+var yye = tgel.getYear();
+
+
+
+
+// ==== Main Async ==== //
+module.exports = iris = async (iris, m, chatUpdate, store) => {
+  try {
+    var body = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectreply.selectedRowId : (m.mtype == 'templateButtonreplyMessage') ? m.message.templateButtonreplyMessage.selectedId : (m.mtype === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectreply.selectedRowId || m.text) : ''
+    var budy = (typeof m.text == 'string' ? m.text : '')
+    const prefix = global.prefa
+    const isCmd = body.startsWith(prefix)
+    const notCmd = body.startsWith('')
+    const command = isCmd ? body.slice(1).trim().split(' ')[0].toLowerCase() : ''
+    const args = body.trim().split(/ +/).slice(1)
+    const pushname = m.pushName || "No Name"
+    const botNumber = await iris.decodeJid(iris.user.id)
+    const isCreator = [botNumber, ...global.Owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+    const itsMe = m.sender == botNumber ? true : false
+    const text = args.join(" ")
+    const from = m.chat
+    const quoted = m.quoted ? m.quoted : m
+    const mime = (quoted.msg || quoted).mimetype || ''
+    const isMedia = /image|video|sticker|audio/.test(mime)
+    const messagesD = body.slice(0).trim().split(/ +/).shift().toLowerCase()
+    const groupMetadata = m.isGroup ? await iris.groupMetadata(m.chat).catch(e => { }) : ''
+    const groupName = m.isGroup ? groupMetadata.subject : ''
+    const participants = m.isGroup ? await groupMetadata.participants : ''
+    const groupAdmins = m.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
+    const groupOwner = m.isGroup ? groupMetadata.owner : ''
+    const isBotAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false
+    const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+    const isUser = pendaftar.includes(m.sender)
+    const isBan = banUser.includes(m.sender)
+    const welcm = m.isGroup ? wlcm.includes(from) : false
+    const isBanChat = m.isGroup ? banchat.includes(from) : false
+    const isRakyat = isCreator || global.rkyt.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender) || false
+    const AntiLink = m.isGroup ? ntilink.includes(from) : false
+    const AntiLinkYoutubeVid = m.isGroup ? ntilinkytvid.includes(from) : false
+    const AntiLinkYoutubeChannel = m.isGroup ? ntilinkytch.includes(from) : false
+    const AntiLinkInstagram = m.isGroup ? ntilinkig.includes(from) : false
+    const AntiLinkFacebook = m.isGroup ? ntilinkfb.includes(from) : false
+    const AntiLinkTiktok = m.isGroup ? ntilinktt.includes(from) : false
+    const AntiLinkTelegram = m.isGroup ? ntilinktg.includes(from) : false
+    const AntiLinkTwitter = m.isGroup ? ntilinktwt.includes(from) : false
+    const AntiLinkAll = m.isGroup ? ntilinkall.includes(from) : false
+    const antiWame = m.isGroup ? ntwame.includes(from) : false
+    const antiVirtex = m.isGroup ? ntvirtex.includes(from) : false
+    const AntiNsfw = m.isGroup ? ntnsfw.includes(from) : false
+    autoreadsw = true
+    const content = JSON.stringify(m.message)
+    const q = args.join(' ')
+    const isQuotedVideo = m.mtype === 'extendedTextMessage' && content.includes('videoMessage')
+    const isQuotedAudio = m.mtype === 'extendedTextMessage' && content.includes('audioMessage')
+    autoreadsw = true;
+    _sewa.expiredCheck(iris, sewa);
+
+    // ====== reply module ====== //
+    const reply = (teks) => {
+      iris.sendMessage(m.chat, { text: teks }, { quoted: m })
+    }
+    
